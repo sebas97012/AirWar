c1a16d602ca2bc9b4752e4106141ecd0999e9dea
sebas97012 <sebas.smr@hotmail.com>
2017-03-20 00:57:32 -0600

    Proyecto base con Overlap2d runtime implementado

---
+++ android/src/com/itcr/ce/airwar/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.itcr.ce.airwar;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.itcr.ce.airwar.AirWar;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AirWar(), config);
+	}
+}
---
+++ desktop/src/com/itcr/ce/airwar/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.itcr.ce.airwar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.itcr.ce.airwar.AirWar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width=1280;
+		config.height=720;
+		new LwjglApplication(new AirWar(), config);
+	}
+}
