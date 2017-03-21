ab09080b5a014379541d2c7c01751d1d07d28f02
Arturo Córdoba <arturocv16@gmail.com>
2017-03-20 18:19:43 -0600

    Inicializado paquete de datos

--- core/src/com/itcr/ce/airwar/AirWar.java
+++ core/src/com/itcr/ce/airwar/AirWar.java
@@ -22,7 +22,7 @@ public class AirWar extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		sceneLoader.getEngine().update(Gdx.graphics.getDeltaTime());
 	}
---
+++ core/src/com/itcr/ce/data/LinkedList.java
@@ -0,0 +1,7 @@
+package com.itcr.ce.data;
+
+/**
+ * Created by Arturo on 20/3/2017.
+ */
+public class LinkedList {
+}
