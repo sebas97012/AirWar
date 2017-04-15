a9304377ad688beb12b23a1ba597b526fd4d91c0
sebas97012 <sebas.smr@hotmail.com>
2017-04-15 04:03:29 -0600

    Basic implementation of server and bridge, wired client connection is working.

---
+++ core/src/com/itcr/ce/airwar/server/Bridge.java
@@ -0,0 +1,55 @@
+package com.itcr.ce.airwar.server;
+
+/**
+ * Created by Sebas Mora on 28/03/2017.
+ */
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+
+public class Bridge {
+
+    public TCPServer Server;
+
+    public Bridge() {
+
+        Server = new TCPServer(new TCPServer.OnMessageReceived() {
+            @Override
+            public void messageReceived(String message) {
+                System.out.print(message);
+                try{
+                    Robot robot = new Robot();
+                    if(message.equals("UP")){
+                        robot.keyPress(KeyEvent.VK_UP);
+                        robot.delay(100);
+                        robot.keyRelease(KeyEvent.VK_UP);
+                    }
+                    else if (message.equals("DOWN")){
+                        robot.keyPress(KeyEvent.VK_DOWN);
+                        robot.delay(100);
+                        robot.keyRelease(KeyEvent.VK_DOWN);
+                    }
+                    else if(message.equals("LEFT")){
+                        robot.keyPress(KeyEvent.VK_LEFT);
+                        robot.delay(100);
+                        robot.keyRelease(KeyEvent.VK_LEFT);
+                    }
+                    else if(message.equals("SPACE")){
+                        robot.keyPress(KeyEvent.VK_SPACE);
+                        robot.delay(100);
+                        robot.keyRelease(KeyEvent.VK_SPACE);
+                    }
+                    else{
+                        robot.keyPress(KeyEvent.VK_RIGHT);
+                        robot.delay(100);
+                        robot.keyRelease(KeyEvent.VK_RIGHT);
+                    }
+                }catch (AWTException e){
+                    e.printStackTrace();}
+            }
+        });
+        Server.start();
+
+    }
+}
