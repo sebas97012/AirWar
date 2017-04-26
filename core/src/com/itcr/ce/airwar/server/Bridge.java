e0b1c5d29b2a7ee39e1a38bd7096eb42e1787673
sebas97012 <sebas.smr@hotmail.com>
2017-04-26 02:37:03 -0600

    Server and Bridge re-optimized

--- core/src/com/itcr/ce/airwar/server/Bridge.java
+++ core/src/com/itcr/ce/airwar/server/Bridge.java
@@ -11,49 +11,51 @@ import java.awt.event.KeyEvent;
 
 
 public class Bridge {
-    private int delay_time = 80;
+    private int delay_time = 20;
     public static TCPServer Server;
+    private static Robot robot;
 
     public Bridge() {
+        try{
+            robot = new Robot();
+        }catch (AWTException e){
+        e.printStackTrace();}
 
         Server = new TCPServer(new TCPServer.OnMessageReceived() {
             @Override
-            public void messageReceived(String message) {
-                System.out.print(message);
-                try{
-                    Robot robot = new Robot();
-                    if(message.equals("u")){
-                        robot.keyPress(KeyEvent.VK_UP);
-                        robot.delay(delay_time);
-                        robot.keyRelease(KeyEvent.VK_UP);
+            public void messageReceived(int message) {
+                    switch (message){
+                        case 1:
+                            robot.keyPress(KeyEvent.VK_UP);
+                            robot.delay(delay_time);
+                            robot.keyRelease(KeyEvent.VK_UP);
+                            break;
+                        case 2:
+                            robot.keyPress(KeyEvent.VK_RIGHT);
+                            robot.delay(delay_time);
+                            robot.keyRelease(KeyEvent.VK_RIGHT);
+                            break;
+                        case 3:
+                            robot.keyPress(KeyEvent.VK_DOWN);
+                            robot.delay(delay_time);
+                            robot.keyRelease(KeyEvent.VK_DOWN);
+                            break;
+                        case 4:
+                            robot.keyPress(KeyEvent.VK_LEFT);
+                            robot.delay(delay_time);
+                            robot.keyRelease(KeyEvent.VK_LEFT);
+                            break;
+                        case 5:
+                            robot.keyPress(KeyEvent.VK_SPACE);
+                            robot.delay(delay_time);
+                            robot.keyRelease(KeyEvent.VK_SPACE);
+                            break;
+                        case 6:
+                            robot.keyPress(KeyEvent.VK_ENTER);
+                            robot.delay(delay_time);
+                            robot.keyRelease(KeyEvent.VK_ENTER);
+                            break;
                     }
-                    else if (message.equals("d")){
-                        robot.keyPress(KeyEvent.VK_DOWN);
-                        robot.delay(delay_time);
-                        robot.keyRelease(KeyEvent.VK_DOWN);
-                    }
-                    else if(message.equals("l")){
-                        robot.keyPress(KeyEvent.VK_LEFT);
-                        robot.delay(delay_time);
-                        robot.keyRelease(KeyEvent.VK_LEFT);
-                    }
-                    else if(message.equals("s")){
-                        robot.keyPress(KeyEvent.VK_SPACE);
-                        robot.delay(delay_time);
-                        robot.keyRelease(KeyEvent.VK_SPACE);
-                    }
-                    else if(message.equals("p")){
-                        robot.keyPress(KeyEvent.VK_ENTER);
-                        robot.delay(delay_time);
-                        robot.keyRelease(KeyEvent.VK_ENTER);
-                    }
-                    else{
-                        robot.keyPress(KeyEvent.VK_RIGHT);
-                        robot.delay(delay_time);
-                        robot.keyRelease(KeyEvent.VK_RIGHT);
-                    }
-                }catch (AWTException e){
-                    e.printStackTrace();}
             }
         });
         Server.start();
