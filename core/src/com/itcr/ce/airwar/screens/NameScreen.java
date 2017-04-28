648e045e4e0d8ce00594d6ecbc723ec8eeafc016
agalvarezsolano <agalvarezsolano@gmail.com>
2017-04-28 12:38:28 -0600

    Asset Screens

--- core/src/com/itcr/ce/airwar/screens/NameScreen.java
+++ core/src/com/itcr/ce/airwar/screens/NameScreen.java
@@ -3,7 +3,6 @@ package com.itcr.ce.airwar.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
